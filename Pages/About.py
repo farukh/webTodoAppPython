003b7a66e24c31a4eaa3934196b0c0ce83ce9bd4
FarukhMushtaq <farukhmushtaq@apesquared.com>
2024-05-11 20:21:05 +0500

    Added About page. Changed Entry page name from web.py to Home.py

---
+++ Pages/About.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.write("<h1>Python Web App </h1>",unsafe_allow_html=True)
+st.write("By: Farukh Mushtaq")
+st.write("farukh.mushtaq@gmail.com")
+st.write("+92-300-0633306")
+
+
+
